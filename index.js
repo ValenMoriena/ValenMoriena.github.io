cf380d2494e2e5dfa7cc9d3f750571d9ff8c2729
ValenMoriena <126523528+ValenMoriena@users.noreply.github.com>
2023-02-27 19:09:06 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("boton_color").addEventListener("click", function () {
+    document.body.style.backgroundColor="#A90551";
+})
+document.getElementById("boton_default").addEventListener("click", function () {
+   document.body.style.backgroundColor="#FFFFFF"
+})
